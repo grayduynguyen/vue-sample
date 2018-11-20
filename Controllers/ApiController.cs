a482e4f4efca90f782d5ce3dbbff623920e20cea
Duy Ng Quang <duynn@goquo.com>
2018-11-20 18:08:11 +0700

    update test redis cache

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace VueSample.Controllers
+{
+    [Route("[controller]/test-cache")]
+    public class ApiController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+        public ApiController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        [Route("get/{cacheKey}")]
+        public string Get(string cacheKey)
+        {
+            if (string.IsNullOrEmpty(cacheKey))
+                return "Failed";
+
+            var value = _distributedCache.GetString(cacheKey);
+            if (!string.IsNullOrEmpty(value))
+                return $"Fetched from cache : {value}";
+
+            value = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+            _distributedCache.SetString(cacheKey, value);
+            return $"Added to cache: {value}";
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using VueSample.Models;
 
 namespace VueSample
@@ -34,6 +28,13 @@ namespace VueSample
             });
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var redisConnection = Configuration.GetConnectionString("RedisConnection");
+            services.AddDistributedRedisCache(option =>
+            {
+                option.Configuration = redisConnection;
+                option.InstanceName = "VueSample_";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
